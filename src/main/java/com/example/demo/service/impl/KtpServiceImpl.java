50a98cbbcf98b5027907c4f3c395266731822f97
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2026-03-13 02:34:04 +0700

    create Ktp service implementation

---
+++ src/main/java/com/example/demo/service/impl/KtpServiceImpl.java
@@ -0,0 +1,52 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.Ktp;
+import com.example.demo.repository.KtpRepository;
+import com.example.demo.service.KtpService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class KtpServiceImpl implements KtpService {
+
+    private final KtpRepository repository;
+
+    public KtpServiceImpl(KtpRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Ktp create(Ktp ktp) {
+        return repository.save(ktp);
+    }
+
+    @Override
+    public List<Ktp> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Ktp getById(Integer id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public Ktp update(Integer id, Ktp ktp) {
+
+        Ktp existing = repository.findById(id).orElseThrow();
+
+        existing.setNomorKtp(ktp.getNomorKtp());
+        existing.setNamaLengkap(ktp.getNamaLengkap());
+        existing.setAlamat(ktp.getAlamat());
+        existing.setTanggalLahir(ktp.getTanggalLahir());
+        existing.setJenisKelamin(ktp.getJenisKelamin());
+
+        return repository.save(existing);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        repository.deleteById(id);
+    }
+}
