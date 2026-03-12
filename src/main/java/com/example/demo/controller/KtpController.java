22880bf5c780e7b7fd1cdac6b0df88d916c95dc5
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2026-03-13 02:36:22 +0700

    Create KTP Controller CRUD API

---
+++ src/main/java/com/example/demo/controller/KtpController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Ktp;
+import com.example.demo.service.KtpService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ktp")
+public class KtpController {
+
+    private final KtpService service;
+
+    public KtpController(KtpService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Ktp create(@RequestBody Ktp ktp) {
+        return service.create(ktp);
+    }
+
+    @GetMapping
+    public List<Ktp> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Ktp getById(@PathVariable Integer id) {
+        return service.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Ktp update(@PathVariable Integer id,
+                      @RequestBody Ktp ktp) {
+        return service.update(id, ktp);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        service.delete(id);
+    }
+}
